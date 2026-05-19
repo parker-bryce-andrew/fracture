59b79c41408d6cfebf54234fbb410c6ee4431b9c
Parker Andrew <parker@andrew.cx>
2026-05-19 05:49:18 -0500

    Revert "attempt at checking if the modifier supports the dmabuf image create with vulkan. it's to (hopefully) improve device compatibility"
    This reverts commit 10ba670f494f11e48ba7bd47d1043ce3e4fd2bda.

--- src/stream_creation/start_mirror_stream.rs
+++ src/stream_creation/start_mirror_stream.rs
@@ -14,10 +14,7 @@ use crate::{
 };
 use drm_fourcc::DrmFourcc;
 use gnome_window_calls::abstraction::{Window, WindowCache};
-use lamco_wgpu::{
-    bridge::SupportedDmaBufImport,
-    smithay_reexports::{self, Dmabuf},
-};
+use lamco_wgpu::smithay_reexports::{self, Dmabuf};
 use mmap::MapOption;
 use pipewire::{
     self as pw,
@@ -796,14 +793,7 @@ pub fn start_mirroring(
 
                 let mods: Vec<_> = v
                     .iter()
-                    .map(|v| {
-                        v.modifiers.iter().map(|v| *v as i64).filter(|modifier| {
-                            v.image_create_mods.contains(&SupportedDmaBufImport {
-                                modifier: *modifier as u64,
-                                vk_format: v.vk_format,
-                            })
-                        })
-                    })
+                    .map(|v| v.modifiers.iter().map(|v| *v as i64))
                     .flatten()
                     .collect();
 
